3539a1e5a63a5def1bbf84338eadee8bcb3e0583
Chuy Mendez <codingchuy@gmail.com>
2021-03-22 23:31:22 -0700

    changed colors and shadows on navbar

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "22px 10px";
-    document.getElementById("navbar").style.background = 'white';
+    document.getElementById("navbar").style.background = '#F0ECE6';
+    document.getElementById("navbar").style.boxShadow = "0 0 5px #585858";
     document.getElementById("title").style.fontSize = "25px";
+    document.getElementById("title").style.color = "black";
   } else {
     document.getElementById("navbar").style.padding = "50px 10px";
     document.getElementById("navbar").style.background = 'transparent';
     document.getElementById("title").style.fontSize = "35px";
+    document.getElementById("title").style.color = "#ffffff";
+    document.getElementById("navbar").style.boxShadow = "none";
   }
 }
 
