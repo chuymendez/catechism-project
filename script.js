e6065493d5f8af41594579ec349f32e5a331ae43
Jesus Mendez <codingchuy@gmail.com>
2021-03-24 16:40:42 -0700

    Added content
    added fonts, content and other adjustments to containers

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "22px 10px";
     document.getElementById("navbar").style.background = '#F0ECE6';
     document.getElementById("navbar").style.boxShadow = "0 0 5px #585858";
-    document.getElementById("title").style.fontSize = "25px";
-    document.getElementById("title").style.color = "black";
+    document.getElementById("title").style.fontSize = "1.5rem";
+    //document.getElementById("title").style.color = "black";
   } else {
     document.getElementById("navbar").style.padding = "50px 10px";
     document.getElementById("navbar").style.background = 'transparent';
-    document.getElementById("title").style.fontSize = "35px";
-    document.getElementById("title").style.color = "#ffffff";
+    document.getElementById("title").style.fontSize = "2.25rem";
+    //document.getElementById("title").style.color = "#ffffff";
     document.getElementById("navbar").style.boxShadow = "none";
   }
 }
